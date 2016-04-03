10392fadfb6ced2d8fc7ed403cb57d37fd2b0ff4
noprom <noprom@nopromdeMacBook-Pro.local>
2016-04-03 17:13:26 +0800

    add 2 sum

---
+++ src/main/java/com/huntdreams/TwoSum/Solution.java
@@ -0,0 +1,15 @@
+package com.huntdreams.TwoSum;
+
+/**
+ * 1. Two Sum
+ * https://leetcode.com/problems/two-sum/
+ * <p/>
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 4/3/16 5:08 PM.
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+    }
+}
