cecb219043d95f6cd3046b7d5f8137bf2e7b967e
noprom <noprom@nopromdeMacBook-Pro.local>
2016-04-03 17:23:13 +0800

    two sum accepted

--- src/main/java/com/huntdreams/TwoSum/Solution.java
+++ src/main/java/com/huntdreams/TwoSum/Solution.java
@@ -10,6 +10,30 @@ package com.huntdreams.TwoSum;
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        boolean flag = false;
+        for (int i = 0; i < length; i++) {
+            if (flag) {
+                break;
+            }
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("result:" + result[0] + "," + result[1]);
     }
 }
