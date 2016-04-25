ffe965c076007741fdf38e0de69b3f593619122f
noprom <tyee.noprom@qq.com>
2016-04-25 17:10:43 +0800

    ac

--- src/main/java/com/huntdreams/ReverseVowelsOfAString/Solution.java
+++ src/main/java/com/huntdreams/ReverseVowelsOfAString/Solution.java
@@ -1,5 +1,9 @@
 package com.huntdreams.ReverseVowelsOfAString;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 345. Reverse Vowels of a String
  * https://leetcode.com/problems/reverse-vowels-of-a-string/
@@ -9,8 +13,41 @@ package com.huntdreams.ReverseVowelsOfAString;
  */
 public class Solution {
 
-    
-    public static void main(String[] args) {
+    private Set<Character> vowels = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
 
+    private boolean isVowels(char c) {
+        return vowels.contains(c);
+    }
+
+    private void swap(char[] s, int i, int j) {
+        char c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+
+    public String reverseVowels(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        char[] seq = s.toCharArray();
+        int start = 0, last = s.length() - 1;
+        while (start < last) {
+            if (!isVowels(seq[start])) {
+                start++;
+            } else if (!isVowels(seq[last])) {
+                last--;
+            } else {
+                swap(seq, start++, last--);
+            }
+        }
+        return new String(seq);
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "hello";
+        s = solution.reverseVowels(s);
+        System.out.println(s);
     }
 }
