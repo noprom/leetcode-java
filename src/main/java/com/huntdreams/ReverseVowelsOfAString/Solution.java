efed05ebe6fc778193e5ce80ed94517dbd7f7533
noprom <tyee.noprom@qq.com>
2016-04-25 17:12:53 +0800

    optimize space

--- src/main/java/com/huntdreams/ReverseVowelsOfAString/Solution.java
+++ src/main/java/com/huntdreams/ReverseVowelsOfAString/Solution.java
@@ -43,7 +43,6 @@ public class Solution {
         return new String(seq);
     }
 
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         String s = "hello";
